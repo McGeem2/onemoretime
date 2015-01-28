c7ea0ff6af1eb128b3d256fca13060a5e3be2d79
McGeem2 <mcgeem2@nku.edu>
2015-01-28 16:52:50 -0500

    ah

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+  	  public static void main(String[] args) {
+		System.out.println("Team member 1 is a jerk!");
+		System.out.println("Team member 1: b");
+		System.out.println("Team member 2 is the best!");
+		System.out.println("Team member 2: 2");
+		System.out.println("I am serious, they are terrible!");
+	   }
+}
